fedbb5ed92ff2302da7d869d859e2ce5445a9baa
osakanaman <shogoyosizawa@gmail.com>
2020-04-30 12:26:48 +0900

    修正

--- homework03.php
+++ homework03.php
@@ -1,12 +1,12 @@
 <?php
 
-function fizzbuzz($a,$b) {
+function fizzbuzz($c,$d) {
     for ($i=1; $i <= 100; $i++) {
-        if ($i % ($a*$b) === 0) {
+        if ($i % ($c*$d) === 0) {
             echo 'FizzBuzz'.PHP_EOL;
-        } elseif ($i % $a === 0) {
+        } elseif ($i % $c === 0) {
             echo 'Fizz'.PHP_EOL;
-        } elseif ($i % $b === 0) {
+        } elseif ($i % $d === 0) {
             echo 'Buzz'.PHP_EOL;
         } else {
             echo $i.PHP_EOL;
