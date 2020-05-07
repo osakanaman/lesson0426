9c67615fb3165f1128347aa1bdfe7495e11b16f8
osakanaman <61353784+osakanaman@users.noreply.github.com>
2020-05-07 17:14:15 +0900

    Update index.php

--- db_lesson/index.php
+++ db_lesson/index.php
@@ -23,19 +23,10 @@ try {
 
 //   $dbh = new PDO("mysql:host=127.0.0.1; dbname=lesson01; charset=utf8", 'hoge', 'hogehoge');
 //   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 //   $sql = "INSERT INTO fish (name, type) VALUES (:name, :type) ";
- 
-//   // 挿入する値は空のまま、SQL実行の準備をする
-//   $stmt = $dbh->prepare($sql);
- 
-//   // 挿入する値を連想配列に格納する
+//   $stmt = $dbh->prepare($sql); 
 //   $fiserman = array('カサゴ'=>'根魚', 'タチウオ'=>'青物');
- 
-//   // foreachで挿入する値を1つずつループ処理
 //   foreach ($fiserman as $key => $val) {
- 
-//   // 連想配列のキーを :name に、値を :population にセットし、executeでSQLを実行
 //   $stmt->execute(array(':name' => $key, ':type' => $val));
 //   }
 // } catch (PDOException $e) {
